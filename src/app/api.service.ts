60630732f2d259b79a37fa2295738b66f207fcd0
kishore951 <kishoresakthivel951@gmail.com>
2022-10-11 21:04:51 +0530

    over

---
+++ src/app/api.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor( private http:HttpClient) { }
+  pubData=()=>{
+    return this.http.get("https://datausa.io/api/data?drilldowns=Nation&measures=Population")
+  }
+}
